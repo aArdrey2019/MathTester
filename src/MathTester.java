68bd3458cb4f0e5f15ea251942471a50c86c25fa
Ashton Ardrey <ashton.ardrey.2019@student.lhschools.org>
2018-08-31 13:45:25 -0400

    Update 1

---
+++ src/MathTester.java
@@ -0,0 +1,21 @@
+public class MathTester {
+
+    public static void main(String [] args){
+
+        String firstWord;
+        String secondWord;
+        int firstNumber,secondNumber;
+
+        firstWord = "Dog";
+        secondWord = "house";
+        firstNumber = 5;
+        secondNumber = 10;
+
+        System.out.println(firstWord+secondWord);
+        System.out.println(firstNumber+firstNumber);
+
+
+
+
+    }
+}
